50512ec36423488027f3f91872c00f910494da62
陳維漢 <wikeyno@gmail.com>
2018-01-03 17:46:47 +0800

    example for CloudSQL command

---
+++ DB_Back-up/test.sql
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+INSERT INTO `employee` (`Emp_UUID`, `Company`, `Department`, `Position`, `Emp_Account`, `Password`, `Emp_Name`, `Img_URL`, `Ses_ID`, `Act_Setting`, `Act_AnotherData`, `Act_Permission`, `Avg_Progress`, `Disable`) VALUES ('', 'C001', '1', '理財寶企劃組/ UI設計師', 'jasmine@cmoney.com.tw', '123456', '黃庭英', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+
+
+
+ 
