af8322b63af4fe3bb666dab30c088ee304fc5875
陳維漢 <wikeyno@gmail.com>
2018-01-04 15:01:59 +0800

    test file update

--- DB_Back-up/test.sql
+++ DB_Back-up/test.sql
@@ -2,10 +2,18 @@
 
 
 
+INSERT INTO `department` (`Dpm_ID`, `Cmp_ID`, `Dpm_Name`, `Dmp_Count`, `Supervisor`, `Description`, `Disable`) VALUES (NULL, 'C001', '大大工程師', '4', '', '大大工程師', '0');
+
+INSERT INTO `employee` (`Emp_UUID`, `Company`, `Department`, `Position`, `Emp_Account`, `Password`, `Emp_Name`, `Img_URL`, `Ses_ID`, `Act_Setting`, `Act_AnotherData`, `Act_Permission`, `Avg_Progress`, `Disable`) VALUES ('', 'C001', '4', '大大工程師/ 學員', 'jeffthompkins83@gmail.com', '123456', '林品均', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
 
 
-INSERT INTO `employee` (`Emp_UUID`, `Company`, `Department`, `Position`, `Emp_Account`, `Password`, `Emp_Name`, `Img_URL`, `Ses_ID`, `Act_Setting`, `Act_AnotherData`, `Act_Permission`, `Avg_Progress`, `Disable`) VALUES ('', 'C001', '1', '理財寶企劃組/ UI設計師', 'jasmine@cmoney.com.tw', '123456', '黃庭英', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+('', 'C001', '4', '大大工程師/ 學員', 'wikeyno@gmail.com', '123456', '陳維漢', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+('', 'C001', '4', '大大工程師/ 學員', 'bggtank0102@gmail.com', '123456', '林庭緯', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+('', 'C001', '4', '大大工程師/ 學員', 'weichu0114@gmail.com', '123456', '廖韋筑', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+('', 'C001', '4', '大大工程師/ 學員', 'daivdxxxcc@gmail.com', '123456', '張辰豪', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+('', 'C001', '4', '大大工程師/ 學員', 'jeffthompkins83@gmail.com', '123456', '林品均', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
+
+('', 'C001', '4', '大大工程師/ 講師', 'spiculate@hotmail.com', '123456', '黃聖方', 'https://vignette.wikia.nocookie.net/peanuts/images/d/dc/Woodstock.gif/revision/latest?cb=20090301022138', 'C001^p^2018Q1', '', '', '0', '0', '0');
 
 
 
- 
