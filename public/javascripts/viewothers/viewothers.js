1e2554ea7eb29c8f1b55eee54d71d4e0b0c8b4fc
Anvis <weichu0114@gmail.com>
2018-01-08 10:20:47 +0800

    edit _showTab = 0

--- public/javascripts/viewothers/viewothers.js
+++ public/javascripts/viewothers/viewothers.js
@@ -281,8 +281,7 @@ function setLoadMoreBtn(str) {
 /* ------ 設定排序依據的分頁 ------ */
 function setTab() {
 	// 預設顯示第一個 Tab
-	var _showTab = 1;
-
+	var _showTab = 0;
 
 	$('.filter_tab').each(function() {
 		// 目前的頁籤區塊
